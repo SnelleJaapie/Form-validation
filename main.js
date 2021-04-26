51490ee8d6e36a2e304796237083f265191c66d6
SnelleJaapie <jasperdekorte@gmail.com>
2021-04-26 13:05:09 +0200

    First Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const usernameContainer = document.getElementById("username")
+const passwordContainer = document.getElementById("password")
+
+const usernameWarning = document.getElementById("username-warning")
+const passwordWarning = document.getElementById("password-warning")
+
