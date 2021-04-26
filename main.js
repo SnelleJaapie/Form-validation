57c7bc6a71cf155d8db05eebc5d72815f8f0ce78
SnelleJaapie <jasperdekorte@gmail.com>
2021-04-26 14:37:27 +0200

    second commit

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const usernameContainer = document.getElementById("username")
-const passwordContainer = document.getElementById("password")
+const username_Input = document.getElementById("usernameInput")
+const password_Input = document.getElementById("passwordInput")
 
-const usernameWarning = document.getElementById("username-warning")
-const passwordWarning = document.getElementById("password-warning")
+const username_Warning = document.getElementById("usernameWarning")
+const password_Warning = document.getElementById("passwordWarning")
+
+username_Input.addEventListener("keyup", checkUsername)
+password_Input.addEventListener("keyup", checkPassWord)
+
+function checkUsername(e) {
+    if (e.target.value.includes("@")) {
+        username_Warning.textContent = "Gebruikersnaam mag geen @ bevatten GVD!!!"
+    } else {
+        username_Warning = ""
+    }
+}
 
